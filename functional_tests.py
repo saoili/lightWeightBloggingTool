2989ea4358077480614374654eb3d96b9e93e0ae
Sorcha Bowler <saoili@gmail.com>
2014-01-26 00:11:51 +0000

    First draft functional tests, comments only

---
+++ functional_tests.py
@@ -0,0 +1,32 @@
+#an outline of the user stories
+#in comments first
+
+
+#write tests for creating users later
+
+
+#Sally wants to post to her blog
+#she goes to the app page and sees that its says 'blog'
+
+#Sally finds the login and password fields 
+#and logs in
+
+#Sally finds the area to input the post and does so
+#Sally publishes her post
+#The website displays a message saying that her post is published
+
+
+
+#write test for deleting blog post
+
+#write test for editing blog post
+
+
+
+
+#Anne wants to read Sally's blog
+#she goes to the blog app page and sees that it says 'blog'
+
+#Anne finds the search function 
+#and searches for Sally's username and hits enter
+#Sally's blog appears
